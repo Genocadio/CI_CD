4b27d9f6e84af6065fc8f4bcdcc2db6c09088a31
geno <yvesgeno@outlook.com>
2024-09-03 19:20:33 +0200

    test5

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ app.get("/version", (req, res) => {
     res.send("1");
 })
 
-// app.get("/health", (req, res) => {
-//     res.send("OK");
-// })
+app.get("/health", (req, res) => {
+    if (true) throw new Error("Error...");
+    res.send("OK");
+})
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
